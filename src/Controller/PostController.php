35022c00bc17dcf091938a7e34fc93877c753a2c
GabrielSOLER1 <gabriel.soler@ynov.com>
2025-12-18 11:14:22 +0100

    Boom

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Form\CommentType;
 use App\Form\PostType;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\Func;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,11 +18,38 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PostController extends AbstractController
 {
     #[Route('/{id}', name: 'app_post_show', methods: ['GET'])]
-    public function show(Post $post): Response
+    public function show(Post $post, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $comment = new Comment();
+        $comment->setAuthor($this->getUser());
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $action = $this->generateUrl('app_post_comment_new', ['id' => $post->getId()]);
+        $form = $this->createForm(CommentType::class, $comment, ['action' => $action]);
+
         return $this->render('post/show.html.twig', [
             'post' => $post,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}/comment/new', methods: ['POST'])]
+    public function comment(Post $post, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $comment = new Comment();
+        $comment->setAuthor($this->getUser());
+        
+        $request->request->set('post', intval($request->get('post')));
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+        }
+        return $this->redirectToRoute('app_post_show', ['id'=> $post->getId()], Response::HTTP_SEE_OTHER);
     }
 
     #[Route('/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
